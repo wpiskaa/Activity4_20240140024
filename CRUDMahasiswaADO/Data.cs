0790f00bc30a42b47b95830a25880410ca9eeb02
wpiskaa <hafizzkurniawan19@gmail.com>
2026-06-20 18:03:37 +0700

    Add Crystal Reports class wrapper and RPT design files

---
+++ CRUDMahasiswaADO/Data.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace CRUDMahasiswaADO
+{
+    public class Data
+    {
+        public string Nama { get; set; }
+        public string JenisKelamin { get; set; }
+        public string Alamat { get; set; }
+        public string NamaProdi { get; set; }
+        public DateTime TanggalDaftar { get; set; }
+    }
+}
